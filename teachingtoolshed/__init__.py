5e527a1c1bf8dc5fafe7aefc07851a8dd51092c7
Hunter Schafer <hschafer@uw.edu>
2023-03-20 20:10:19 -0700

    Bugfix: Wrong package name 😢

--- teachingtoolshed/__init__.py
+++ teachingtoolshed/__init__.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version("teachingtoolshed")
+__version__ = importlib.metadata.version("teaching-toolshed")
 
 from . import api
 from . import gradebook
